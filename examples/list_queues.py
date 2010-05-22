596ca05a120fc13c6a6682c8507d873a1cae1892
Alex Clemesha <clemesha@ucsd.edu>
2010-05-21 17:24:02 -0700

    - New examples using RabbitMQControlService in 'examples'. - Moved 'twotp' specific examples into their own subdir of 'examples'. - Update TODO with more ideas of what to improve next.

---
+++ examples/list_queues.py
@@ -0,0 +1,42 @@
+from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from txrabbitmq.service import RabbitMQControlService
+from twotp.node import Process, readCookie, buildNodeName
+
+
+def rabbitmqctl_client(erlang_cookie=None, nodename="txrabbitmq"):
+    cookie = readCookie() #TODO: allow passing 'erlang_cookie'
+    nodeName = buildNodeName(nodename)
+    process = Process(nodeName, cookie)
+    return RabbitMQControlService(process)
+
+@inlineCallbacks
+def list_queues():
+    client = rabbitmqctl_client()
+    allqueues = yield client.list_queues()
+    for q in allqueues["result"]:
+        print "\n ", q, "\n"
+    returnValue(allqueues["result"])
+
+
+@inlineCallbacks
+def queued_messages(queue_name):
+    """
+    Returns the number of existing messages in queue 'queue_name'.
+    Returns -1 if 'queue_name' does not exist.
+    """
+    client = rabbitmqctl_client()
+    allqueues = yield client.list_queues()
+    for q in allqueues["result"]:
+        if q[0] == queue_name:
+            print q[1]["messages"]
+            returnValue(q[1]["messages"])
+    returnValue(-1) #'queue_name' was not found.
+
+
+
+#reactor.callWhenRunning(list_queues)
+reactor.callWhenRunning(queued_messages, "stocks")
+reactor.run()
+
